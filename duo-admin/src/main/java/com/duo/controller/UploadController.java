2d672bdd6b51ba31217976c18c92205ef1428d77
HCDUO <2650591287@qq.com>
2023-08-10 22:51:25 +0800

    上传文件修改

--- duo-admin/src/main/java/com/duo/controller/UploadController.java
+++ duo-admin/src/main/java/com/duo/controller/UploadController.java
@@ -1,15 +1,16 @@
 package com.duo.controller;
 
-import com.duo.annotation.SystemLog;
 import com.duo.domain.ResponseResult;
 import com.duo.service.UploadService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
 
 /**
  * <pre>
@@ -27,11 +28,19 @@ public class UploadController {
     private UploadService uploadService;
 
     @PostMapping("/upload")
-    @SystemLog(BusinessName = "上传文件")
     @ApiOperation(value = "上传文件",notes = "上传文件")
-    public ResponseResult uploadImg(@RequestParam("img") MultipartFile multipartFile) {
+    public ResponseResult uploadImg(MultipartHttpServletRequest request) {
         try {
-            return uploadService.uploadImg(multipartFile);
+            // 从请求中获取名为 "img" 的文件列表
+            List<MultipartFile> files = request.getFiles("img");
+
+            if (files != null && !files.isEmpty()) {
+                MultipartFile multipartFile = files.get(0); // 假设只上传了一个文件
+                // 调用uploadService.uploadImg方法，并传递MultipartFile类型的参数
+                return uploadService.uploadImg(multipartFile);
+            } else {
+                throw new IllegalArgumentException("没有找到上传的文件");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("文件上传上传失败");
--- duo-blog/src/main/java/com/duo/controller/UploadController.java
+++ duo-blog/src/main/java/com/duo/controller/UploadController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
 
 /**
  * <pre>
@@ -25,9 +28,22 @@ public class UploadController {
     private UploadService uploadService;
 
     @PostMapping("/upload")
-    @SystemLog(BusinessName = "更新个人信息")
     @ApiOperation(value = "更新个人信息",notes = "更新个人信息")
-    public ResponseResult uploadImg(MultipartFile img){
-        return uploadService.uploadImg(img);
+    public ResponseResult uploadImg(MultipartHttpServletRequest request) {
+        try {
+            // 从请求中获取名为 "img" 的文件列表
+            List<MultipartFile> files = request.getFiles("img");
+
+            if (files != null && !files.isEmpty()) {
+                MultipartFile multipartFile = files.get(0); // 假设只上传了一个文件
+                // 调用uploadService.uploadImg方法，并传递MultipartFile类型的参数
+                return uploadService.uploadImg(multipartFile);
+            } else {
+                throw new IllegalArgumentException("没有找到上传的文件");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("文件上传上传失败");
+        }
     }
 }
