8ac5332fdaf6a80e9f10826a786f74badf825d32
HCDUO <2650591287@qq.com>
2023-11-29 15:08:34 +0800

    修改服务器地址

--- duo-framework/src/main/java/com/duo/annotation/SystemLog.java
+++ duo-framework/src/main/java/com/duo/annotation/SystemLog.java
@@ -8,6 +8,9 @@ import java.lang.annotation.Target;
 /**
  * <pre>
  *  用于记录日志
+ *  @Retention(RetentionPolicy.RUNTIME)：此行表示该注解应该在运行时保留，以便通过反射发现和使用。
+ *
+ *  @Target(ElementType.METHOD)：此行指定注解只能应用于方法。
  * </pre>
  *
  * @author <a href="https://github.com/HCDUO">HCDUO</a>
