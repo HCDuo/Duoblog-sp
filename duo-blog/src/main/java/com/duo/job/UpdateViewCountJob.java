a2f9aeb02ada16845ab6c9bbadb05ad04b6c871c
HCDUO <2650591287@qq.com>
2023-12-19 13:38:59 +0800

    定时任务改为一小时刷新

--- duo-blog/src/main/java/com/duo/job/UpdateViewCountJob.java
+++ duo-blog/src/main/java/com/duo/job/UpdateViewCountJob.java
@@ -1,15 +1,12 @@
 package com.duo.job;
 
-import com.duo.domain.entity.Article;
 import com.duo.service.ArticleService;
 import com.duo.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * <pre>
@@ -29,7 +26,7 @@ public class UpdateViewCountJob {
     @Autowired
     private ArticleService articleService;
 
-    @Scheduled(cron = "0/5 * * * * ?")
+    @Scheduled(cron = "0 0 * * * ?")
     public void updateViewCount(){
         //获取redis中的浏览量
         Map<String, Integer> viewCountMap = redisCache.getCacheMap("article:viewCount");
